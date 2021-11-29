d867d7a028bf7073b2e4d93f24c3c0c8b28e86ff
prateek0803 <prateekchatterjee622@gmail.com>
2021-11-29 17:57:38 +0530

    added queue methods

---
+++ queue.js
@@ -0,0 +1,47 @@
+class Node{
+    constructor(val){
+        this.val    = val;
+        this.next   = null;
+    }
+}
+
+class Queue{
+    constructor(){
+        this.first  = null;
+        this.last   = null;
+        this.size   = 0
+    }
+
+    enqueue(val){
+        let newNode = new Node(val)
+        if(!this.first){
+            this.first  = newNode
+            this.last   = newNode
+        }else{
+            this.last.next  = newNode
+            this.last       = newNode
+        }
+
+        return this.size++
+    }
+
+    dequeue(){
+        if(!this.first) return null
+        let temp    = this.first;
+        if(this.last === this.first)  return this.last = null
+
+        this.first  = this.first.next
+        this.size --
+        return temp.val
+    }
+}
+
+
+
+let queue = new Queue
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+console.log(queue)
+queue.dequeue()
+console.log(queue)
