ee3eb9841bf9b9aad21f0a02b65638a0fa309a90
Prateek 0803 <40174790+Prateek0803@users.noreply.github.com>
2022-05-02 08:46:43 +0530

    Time: 86 ms (83.91%), Space: 52.1 MB (24.92%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -3,18 +3,16 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let minVal  = prices[0]
-    let maxDiff = 0
-    
-    for(let i = 1; i < prices.length; i++ ){
+    let minVal = prices[0]
+    let ans = 0
+    for(let i=1;i<prices.length;i++){
+        minVal = Math.min(prices[i],minVal);
         
-        minVal = Math.min(prices[i],minVal)
+        let currDif = prices[i] - minVal;
         
-        let currMin = prices[i] - minVal
-        
-        if(currMin != maxDiff){
-            maxDiff = Math.max(currMin, maxDiff)
+        if(currDif != ans){
+            ans = Math.max(currDif,ans)
         }
     }
-    return maxDiff;
+    return ans
 };
