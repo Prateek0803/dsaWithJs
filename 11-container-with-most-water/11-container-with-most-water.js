b0c29794b643a770abee08b783961b8a6568bb09
Prateek 0803 <40174790+Prateek0803@users.noreply.github.com>
2022-05-02 08:07:17 +0530

    Time: 79 ms (83.98%), Space: 49.2 MB (91.00%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length-1;
+    let ans = 0;
+    
+    while(left <= right){
+        let h = Math.min(height[left],height[right]);
+        let prod = h*(right-left);
+        
+        if(prod > ans){
+            ans = prod;
+        }
+        
+        if(height[left] < height[right]){
+            left++;
+        }else{
+            right--
+        }
+    }
+    return ans
+};
