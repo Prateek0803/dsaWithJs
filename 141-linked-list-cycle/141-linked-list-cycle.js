b6d0d4f61d07de28fa6e3e3ee77754da01acd6bf
Prateek 0803 <40174790+Prateek0803@users.noreply.github.com>
2022-05-18 12:31:45 +0530

    Time: 80 ms (80.71%), Space: 45.2 MB (32.95%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let fastPtr = head;
+    let slowPtr = head;
+    while(fastPtr && fastPtr.next){
+        fastPtr = fastPtr.next.next
+        slowPtr = slowPtr.next
+        
+        if(fastPtr === slowPtr) return true
+    }
+    
+    return false;
+    
+};
